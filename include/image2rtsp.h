a987cd3bff70c114cfc4dd493e64d18426dcd448
erik <erik@panda>
2013-12-14 18:24:44 +0100

    now everything in a single gstreamer pipeline, avoiding the extra copy over udp

--- include/image2rtsp.h
+++ include/image2rtsp.h
@@ -5,15 +5,11 @@ namespace image2rtsp {
 	class Image2RTSPNodelet : public nodelet::Nodelet {
 		public:
 			void onInit();
-			~Image2RTSPNodelet();
-			void rtsp_server_add_url(char *url, int port, char *caps);
 			void url_connected(std::string url);
 			void url_disconnected(std::string url);
-			int init_count;
 
 		private:
 			ros::Subscriber sub_rgb, sub_ir;
-			GstElement *pipeline_rgb, *pipeline_ir;
 			GstAppSrc *appsrc_rgb, *appsrc_ir;
 			GstRTSPServer *rtsp_server;
 			int num_rgb, num_ir;
@@ -21,9 +17,10 @@ namespace image2rtsp {
 			void rgbCallback(const sensor_msgs::Image::ConstPtr& msg);
 			void irCallback(const sensor_msgs::Image::ConstPtr& msg);
 			void video_mainloop_start();
-			GstElement *video_start(char *spipeline, GstAppSrc **appsrc, char *url, char *caps, int port);
-			void video_stop(GstElement *pipeline, GstAppSrc *appsrc);
+			void rtsp_server_add_url(char *url, char *sPipeline, GstElement **appsrc);
 			GstRTSPServer *rtsp_server_create();
+			void set_appsrc_rgb(GstAppSrc *appsrc);
+			void set_appsrc_ir(GstAppSrc *appsrc);
 	};
 }
 
--- src/video.cpp
+++ src/video.cpp
@@ -14,35 +14,6 @@
 using namespace std;
 using namespace image2rtsp;
 
-/**
- * Receives asynchronous messages type GST_MESSAGE_ERROR in the main loop
- * and prints them on stderr.
- */
-static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data) {
-	gchar *debug;
-	GError *error;
-
-	switch (GST_MESSAGE_TYPE(msg)) {
-		case GST_MESSAGE_ERROR:
-			gst_message_parse_error(msg, &error, &debug);
-			g_free(debug);
-
-			printf("Error: %s\n", error->message);
-			g_error_free(error);
-			break;
-                case GST_MESSAGE_WARNING:
-                        gst_message_parse_warning(msg, &error, &debug);
-                        g_free(debug);
-                        printf("Warning: %s\n", error->message);
-                        g_error_free(error);
-                        break;
-		default:
-			break;
-	}
-
-	return TRUE;
-}
-
 
 static void *mainloop(void *arg) {
 	GMainLoop *loop = g_main_loop_new(NULL, FALSE);
@@ -93,12 +64,18 @@ GstRTSPServer *Image2RTSPNodelet::rtsp_server_create() {
 }
 
 
-void Image2RTSPNodelet::rtsp_server_add_url(char *url, int port, char *caps) {
+/* called when a new media pipeline is constructed. We can query the
+ * pipeline and configure our appsrc */
+static void media_configure(GstRTSPMediaFactory *factory, GstRTSPMedia *media, GstElement **appsrc)
+{
+	GstElement *pipeline = media->pipeline;
+	*appsrc = gst_bin_get_by_name(GST_BIN(pipeline), "imagesrc");
+}
+
+
+void Image2RTSPNodelet::rtsp_server_add_url(char *url, char *sPipeline, GstElement **appsrc) {
 	GstRTSPMediaMapping *mapping;
 	GstRTSPMediaFactory *factory;
-	char sPipeline[512];
-
-	snprintf(sPipeline, 512, "( udpsrc port=%d caps=\"%s\" ! rtph264depay ! rtph264pay pt=96 name=pay0 config-interval=1 )", port, caps);
 
 	/* get the mapping for this server, every server has a default mapper object
 	 * that be used to map uri mount points to media factories */
@@ -111,6 +88,10 @@ void Image2RTSPNodelet::rtsp_server_add_url(char *url, int port, char *caps) {
 	factory = gst_rtsp_media_factory_new();
 	gst_rtsp_media_factory_set_launch(factory, sPipeline);
 
+        /* notify when our media is ready, This is called whenever someone asks for
+	 * the media and a new pipeline is created */
+	g_signal_connect(factory, "media-configure", (GCallback)media_configure, appsrc);
+
 	gst_rtsp_media_factory_set_shared(factory, TRUE);
 
 	/* attach the test factory to the /test url */
@@ -118,104 +99,4 @@ void Image2RTSPNodelet::rtsp_server_add_url(char *url, int port, char *caps) {
 
 	/* don't need the ref to the mapper anymore */
 	g_object_unref(mapping);
-}
-
-
-static void notify_caps(GstPad *pad, GParamSpec *pspec, Image2RTSPNodelet *nodelet) {
-	GstCaps *caps;
-	char *pad_caps;
-	char escaped_caps[320];
-	char *c_src;
-	char *c_dest;
-	char *url = (char *)g_object_get_data(G_OBJECT(pad), "param_url");
-	int port = (int)g_object_get_data(G_OBJECT(pad), "param_port");
-
-#if GST_VERSION_MAJOR > 0
-	caps = gst_pad_get_current_caps(pad);
-#else
-	caps = gst_pad_get_negotiated_caps(pad);
-#endif
-	pad_caps = gst_caps_to_string(caps);
-
-	for(c_src=pad_caps, c_dest=escaped_caps;; c_src++) {
-		// Escape some characters
-		if (*c_src == '\\') {
-			*c_dest = '\\';
-			c_dest++;
-		} else if (*c_src == '"') {
-			*c_dest = '\\';
-			c_dest++;
-		}
-		
-		*c_dest = *c_src;
-		c_dest++;
-
-		// EOS
-		if (*c_src == '\0') break;
-		else if(c_dest-escaped_caps >= 320) {
-			fprintf(stderr, "Fixme: Caps string to short: Need %d bytes\n", strlen(pad_caps));
-			exit(1);
-		}
-	}
-
-	printf("Got caps %s\n", pad_caps);
-	nodelet->rtsp_server_add_url(url, port, escaped_caps);
-	nodelet->url_disconnected(url);
-	nodelet->init_count++;
-}
-
-
-GstElement *Image2RTSPNodelet::video_start(char *spipeline, GstAppSrc **appsrc, char *url, char *caps, int port) {
-	GError *error=NULL;
-	GstBus *bus;
-	GstElement *udpsink;
-	GstStateChangeReturn ret;
-	GstElement *pipeline;
-
-	pipeline = gst_parse_launch(spipeline, &error);
-
-	if (!pipeline) {
-		printf("Pipeline could not be created. Exiting.\n");
-		return NULL;
-	} else if (error != NULL) {
-		printf("Error: %s\n", error->message);
-		g_error_free(error);
-		return NULL;
-	}
-
-	udpsink = gst_bin_get_by_name(GST_BIN(pipeline), "udpsink0");
-	if (udpsink) {
-		GstPad *pad = gst_element_get_static_pad(udpsink, "sink");
-		g_object_set_data(G_OBJECT(pad), "param_url", url);
-		g_object_set_data(G_OBJECT(pad), "param_port", (void *)port);
-
-		g_signal_connect(pad, "notify::caps", G_CALLBACK(notify_caps), this);
-		gst_object_unref(udpsink);
-	}
-
-	// add error handler	
-	bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
-	gst_bus_add_watch(bus, bus_call, NULL);
-	gst_object_unref(bus);
-
-        *appsrc = (GstAppSrc *)gst_bin_get_by_name(GST_BIN(pipeline), "appsrc0");
-        if (*appsrc != NULL) {
-                g_object_set(G_OBJECT(*appsrc), "caps", gst_caps_from_string(caps), NULL);
-        }
-
-
-	ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-	if (ret == GST_STATE_CHANGE_FAILURE) {
-		printf("Unable to set the pipeline to the playing state.\n");
-		gst_element_set_state(pipeline, GST_STATE_NULL);
-	}
-
-	return pipeline;
-}
-
-
-void Image2RTSPNodelet::video_stop(GstElement *pipeline, GstAppSrc *appsrc) {
-	gst_element_set_state(pipeline, GST_STATE_NULL);
-	gst_object_unref(GST_OBJECT(pipeline));
-	gst_object_unref(GST_OBJECT(appsrc));
 }
